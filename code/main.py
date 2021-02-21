927f270efc62a95b3e619ba118bf62d902715975
Nakamura Yuji <57951904+Yuji-207@users.noreply.github.com>
2021-02-21 19:22:54 +0900

    音源と○×を追加

--- code/main.py
+++ code/main.py
@@ -1,13 +1,14 @@
 import csv
 import time
 import tkinter as tk
+# from playsound import playsound
 
 class Application(tk.Frame):
     def __init__(self,master):
         super().__init__(master)
         self.pack()
 
-        master.geometry("500x500")
+        master.geometry("600x300")
         master.title("ストップウォッチ")
         master.config(bg="black")
 
@@ -16,16 +17,20 @@ class Application(tk.Frame):
         self.elapsedTime=0.0
         self.lap1_times = []
         self.lap2_times = []
+        self.scores = []
         self.playTime=False
+        self.sound_path = '/Users/yuji/Documents/GitHub/stopwatch/sound/fasten_seatbelt_sign.wav'
+        self.csv_path = '/Users/yuji/Documents/study_csv.csv'
 
-        self.canvas = tk.Canvas(master,width=490,height=80,bg="skyblue")
+        self.canvas = tk.Canvas(master,width=590,height=80,bg="skyblue")
         self.canvas.place(x=3,y=10)
 
         tk.Button(master,text="Reset",command=self.resetButtonClick,width=10).place(x=10, y=110)
         tk.Button(master,text="Start",command=self.startButtonClick,width=10).place(x=110, y=110)
         tk.Button(master,text="Stop",command=self.stopButtonClick,width=10).place(x=210, y=110)
-        tk.Button(master,text="Lap 1",command=self.lap1ButtonClick,width=10).place(x=310, y=110)
-        tk.Button(master,text="Lap 2",command=self.lap2ButtonClick,width=10).place(x=410, y=110)
+        tk.Button(master,text="Correct",command=self.correctButtonClick,width=10).place(x=310, y=110)
+        tk.Button(master,text="Incorrect",command=self.incorrectButtonClick,width=10).place(x=410, y=110)
+        tk.Button(master,text="Next",command=self.nextButtonClick,width=10).place(x=510, y=110)
 
         master.after(50,self.update)
 
@@ -33,31 +38,36 @@ class Application(tk.Frame):
         if not self.playTime:
             self.startTime=time.time()-self.elapsedTime
             self.playTime=True
+            # playsound(self.sound_path)
 
     def stopButtonClick(self):
         if self.playTime:
             self.stopTime=time.time()-self.startTime
             self.playTime=False
 
-    def lap1ButtonClick(self):
+    def nextButtonClick(self):
         if self.playTime:
             self.lap1_time = time.time() - self.startTime
             self.lap1_times.append(self.lap1_time)
-            lap1 = tk.Label(text=self.lap1_time)
-            lap1.pack()
+            lap = tk.Label(text=self.lap1_time)
+            lap.pack()
 
-    def lap2ButtonClick(self):
+    def correctButtonClick(self):
         if self.playTime:
             self.lap2_time = time.time() - self.startTime
             self.lap2_times.append(self.lap2_time)
-            lap2 = tk.Label(text=self.lap2_time)
-            lap2.pack()
+            self.scores.append(True)
+
+    def incorrectButtonClick(self):
+        if self.playTime:
+            self.lap2_time = time.time() - self.startTime
+            self.lap2_times.append(self.lap2_time)
+            self.scores.append(False)
 
     def resetButtonClick(self):
-        path = '/Users/yuji/Documents/study_csv.csv'
-        with open(path, 'w') as f:
+        with open(self.csv_path, 'w') as f:
             writer = csv.writer(f)
-            writer.writerows([self.lap1_times, self.lap2_times].T)
+            writer.writerows([self.lap1_times, self.lap2_times, self.scores])
         self.startTime=time.time()
         self.stopTime=0.0
         self.elapsedTime=0.0
@@ -67,9 +77,9 @@ class Application(tk.Frame):
         self.canvas.delete("Time")
         if self.playTime:
             self.elapsedTime=time.time()-self.startTime
-            self.canvas.create_text(480,40,text=round(self.elapsedTime,1),font=("Helvetica",40,"bold"),fill="black",tag="Time",anchor="e")
+            self.canvas.create_text(580,40,text=round(self.elapsedTime,1),font=("Helvetica",40,"bold"),fill="black",tag="Time",anchor="e")
         else:
-            self.canvas.create_text(480,40,text=round(self.stopTime,1),font=("Helvetica",40,"bold"),fill="black",tag="Time",anchor="e")
+            self.canvas.create_text(580,40,text=round(self.stopTime,1),font=("Helvetica",40,"bold"),fill="black",tag="Time",anchor="e")
 
         self.master.after(50,self.update)
 
