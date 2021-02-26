047864b337be02839c2cf1047d11f09d3ca97c74
Nakamura Yuji <57951904+Yuji-207@users.noreply.github.com>
2021-02-26 22:40:55 +0900

    correct/incorrect/next機能と集計機能を追加

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ let stopTime = 0;
 let count = false;
 let intervalId;
 let diff;
+let scores = [];
+let number = 0;
+let sumScore = 0;
 
 document.getElementById('start').onclick = () => {
   if (count == false) {
@@ -11,7 +14,7 @@ document.getElementById('start').onclick = () => {
 
     intervalId = setInterval(() => {
       diff = Date.now() - startTime;
-      document.querySelector('#display').innerHTML = Math.ceil(diff / 1000);
+      document.getElementById('display').innerHTML = Math.ceil(diff / 1000);
     });
 
   }
@@ -28,6 +31,54 @@ document.getElementById('stop').onclick = () => {
 document.getElementById('reset').onclick = () => {
   count = false;
   stopTime = 0;
+  scores = [];
+  number = [];
   clearInterval(intervalId);
-  document.querySelector('#display').innerHTML = 0;
+  document.getElementById('display').innerHTML = 0;
+  document.getElementById('scores').innerHTML = '';
+};
+
+document.getElementById('correct').onclick = () => {
+  if (count == true) {
+
+    scores.push(true);
+    document.getElementById('scores').insertAdjacentHTML('beforeend', '○');
+
+  }
+};
+
+document.getElementById('incorrect').onclick = () => {
+  if (count == true) {
+
+    scores.push(false);
+    document.getElementById('scores').insertAdjacentHTML('beforeend', '×');
+  
+  }
+};
+
+document.getElementById('next').onclick = () => {
+  if (count == true) {
+
+    let sum = scores.reduce(function(a, b){
+      return a + b;
+    });
+
+    sumScore += sum;
+
+    let score = sum / scores.length * 100;
+    if (score < 100) {
+      score = score.toPrecision(2);
+    }
+
+    scores = [];
+    number++;
+
+    let minute = diff / number / 60000;
+    minute = minute.toPrecision(2);
+    
+    document.getElementById('scores').insertAdjacentHTML('beforeend', ' ' + String(score) + '%');
+    document.getElementById('scores').insertAdjacentHTML('beforeend', ' ' + String(minute) + ' m/q');
+    document.getElementById('scores').insertAdjacentHTML('beforeend', '<br>');
+
+  }
 };
